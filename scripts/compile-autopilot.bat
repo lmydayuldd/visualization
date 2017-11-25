04e1721cf610738e5896fbd4cb6f5403b2a3f3c8
Alexander Ryndin <alexander.ryndin@inform-software.com>
2017-11-25 22:52:33 +0100

    cleanup: removed binaries (DLL) from source control

--- scripts/compile-autopilot.bat
+++ scripts/compile-autopilot.bat
@@ -8,7 +8,7 @@ g++ -shared -fPIC -I"%PATH_TO_JDK_HOME%\include" ^
    -I"%PATH_TO_JDK_HOME%\include\win32" ^
    -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1" ^
    -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1\octave" ^
-   -L"..\octave-libs" ^
+   -L"%PATH_TO_OCTAVE_HOME%\bin" ^
    -o "%OUTPUT_DIR%\AutopilotAdapter.dll" ^
    ..\src\main\cpp\autopilot-adapter\AutopilotAdapter.cpp ^
    -loctave-4 -loctinterp-4
--- scripts/compile-tests-executable.bat
+++ scripts/compile-tests-executable.bat
@@ -6,7 +6,7 @@ if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
 g++ -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1" ^
    -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1\octave" ^
-   -L"..\octave-libs" ^
+   -L"%PATH_TO_OCTAVE_HOME%\bin" ^
    -o "%OUTPUT_DIR%\tests.exe" ^
    ..\src\main\cpp\autopilot\test\tests_main.cpp ^
    -loctave-4 -loctinterp-4
