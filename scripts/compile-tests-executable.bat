66d5ea7ec8fa967492d8b576369a58e1f05c1f77
Alexander Ryndin <alexander.ryndin@inform-software.com>
2017-11-25 18:27:22 +0100

    impl. catch2-based tests generation

---
+++ scripts/compile-tests-executable.bat
@@ -0,0 +1,12 @@
+call variables.bat
+
+set OUTPUT_DIR=..\target\lib
+
+if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+
+g++ -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1" ^
+   -I"%PATH_TO_OCTAVE_HOME%\include\octave-4.2.1\octave" ^
+   -L"..\octave-libs" ^
+   -o "%OUTPUT_DIR%\tests.exe" ^
+   ..\src\main\cpp\autopilot\test\tests_main.cpp ^
+   -loctave-4 -loctinterp-4
